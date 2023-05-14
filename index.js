a2e8016060b6133168240fbe60a537a2e7dee0a5
esha98 <eshashah3335@gmail.com>
2023-05-14 12:45:29 +1000

    Middleware and Error handling

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = 3000;
 // Using public folder
 app.use('/images', express.static('public'))
 
+// Using express.json and express.urlencoded
+//app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // Get - redirect method
 app.get('/redirect', (req, res) => {
   res.redirect('http://www.linkedin.com')
@@ -16,7 +20,7 @@ app.get('/redirect', (req, res) => {
 // Route Chaining 
 app.route("/class")
     .get((req, res) => {
-        res.send('Retrive Class Info')
+        throw new Error();
     })
     .post((req, res) => {
         res.send("Create Class Info")
@@ -34,13 +38,20 @@ app.get('/', (req, res, next) => {
  })
  
 app.get('/class/:id', (req, res) => {
+   // Middleware: Access the routing Parameter  
    const studentID = Number(req.params.id);
 
    const student = data.filter((student) => student.id === studentID);
 
+   // Everything above this line is middleware
    res.send(student);
-   console.log('student: ', student);
-   console.log('params -->', studentID);
+})
+
+// POST - express.json and express.urlencoded
+app.post('/item', (req,res) => {
+    console.log(req.body);
+
+    res.send(req.body);
 })
 
 app.post('/create', (req, res) => {
@@ -51,6 +62,20 @@ app.put('/update', (req, res) => {
     res.send('This is a PUT request at /update')
 })
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    res.status(500).send('Something is Broken!!')
+})
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on ${PORT}`);
 })
+
+/** Build-In Middleware Functions
+  
+ express.static     : Serverless static assets
+ express.json       : Parses incoming requests with JSON payloads
+ express.urlencoded : Parses incoming requests with URL-encoded payloads
+ 
+ */
